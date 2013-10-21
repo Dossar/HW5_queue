523de5547a3d8e072f2d5da2b6442cc32ae8c521
mikemeding <mikeymeding@gmail.com>
2013-10-21 10:48:43 -0400

    added comments to header file. all we need now is a get front function and a airplane.c implementation -MM

--- airplane_queue/queue.h
+++ airplane_queue/queue.h
@@ -13,7 +13,7 @@ extern "C" {
 #endif
 
 #include <stdlib.h> 
-
+    
     typedef struct node {
         QueueData data;
         struct node *next;
@@ -23,42 +23,51 @@ extern "C" {
         NodePtr head, tail;
     } QueueType, *Queue;
 
+    /*Sets up and initializes the queue*/
     Queue initQueue() {
-        Queue qp = (Queue) malloc(sizeof (QueueType));
+        Queue qp = (Queue) malloc(sizeof (QueueType)); // allocate base size of queue
+        // set head and tail to null
         qp -> head = NULL;
         qp -> tail = NULL;
-        return qp;
+        return qp; // return our new queue
+    }
+    
+    /*Decides if the queue is empty or not*/
+    int isEmpty(Queue Q) {
+        return (Q -> head == NULL); // check if head is null
     }
 
-    int empty(Queue Q) {
-        return (Q -> head == NULL);
-    }
-
+    /*Add a value to the tail of the queue*/
     void enqueue(Queue Q, QueueData d) {
-        NodePtr np = (NodePtr) malloc(sizeof (Node));
+        NodePtr np = (NodePtr) malloc(sizeof (Node)); // allocate needed space for new value
+        // add data to this new value
         np -> data = d;
         np -> next = NULL;
-        if (empty(Q)) {
-            Q -> head = np;
+        
+        if (isEmpty(Q)) { // check if queue is empty first
+            // if it is empty head and tail are the same.
+            Q -> head = np; 
             Q -> tail = np;
         } else {
+            // otherwise set only the tail
             Q -> tail -> next = np;
             Q -> tail = np;
         }
     }
 
+    /*Remove a value from the head of the queue*/
     QueueData dequeue(Queue Q) {
-        if (empty(Q)) {
+        if (isEmpty(Q)) { // check if its empty first
             printf("\nAttempt to remove from an empty queue\n");
-            exit(1);
+            exit(1); // exits the program here (perhaps add error case?)
         }
 
-        QueueData hold = Q -> head -> data;
-        NodePtr temp = Q -> head;
-        Q -> head = Q -> head -> next;
-        if (Q -> head == NULL) Q -> tail = NULL;
-        free(temp);
-        return hold;
+        QueueData hold = Q -> head -> data; // create temp of current data
+        NodePtr temp = Q -> head; // create temp of the entire head node
+        Q -> head = Q -> head -> next; // push the head of the queue to the next value
+        if (Q -> head == NULL) Q -> tail = NULL; // if head is now null then so is tail
+        free(temp); // free associated memory
+        return hold; // return our temp data
     }
 
 

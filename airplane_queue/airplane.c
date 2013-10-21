14c72d8c94d71a4cf0cbb84cada4e24ca42fffaa
mikemeding <mikeymeding@gmail.com>
2013-10-21 10:31:42 -0400

    Added queue.h library and will comment soon. created project and set up basic structure. -MM

---
+++ airplane_queue/airplane.c
@@ -0,0 +1,24 @@
+/* 
+ * File:   airplane.c
+ * Author: mike
+ *
+ * Created on October 21, 2013, 10:26 AM
+ */
+
+#include <stdlib.h>
+#include <stdio.h> 
+
+typedef struct {     
+    int num;
+} QueueData;
+
+#include "queue.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
